863c29e58cabb89073cda91473790ed2fd0f1403
yoshikazu hotta <hotta-nagoya-6163@au.com>
2022-07-17 08:25:52 +0900

    htmlファイルを表示

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!!");
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+}); 
 
 app.listen(3000, () => {
   console.log("Start server port:3000");
